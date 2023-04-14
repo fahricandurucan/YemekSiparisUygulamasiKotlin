53e8fdcf8cc2eedadb9e46d0a98855ed7efc7f90
fahricandurucan <fahricandurucan@icloud.com>
2023-04-14 11:15:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/yemeksiparisuygulamasikotlin/ui/fragment/SepetSayfasiFragment.kt
@@ -0,0 +1,15 @@
+package com.example.yemeksiparisuygulamasikotlin.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.yemeksiparisuygulamasikotlin.R
+
+class SepetSayfasiFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_sepet_sayfasi, container, false)
+    }
+
+}
