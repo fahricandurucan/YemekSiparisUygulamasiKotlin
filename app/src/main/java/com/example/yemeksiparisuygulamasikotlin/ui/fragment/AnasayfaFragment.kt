8ec8d1eca0f3195eeb7865890aa4f7264d591123
fahricandurucan <fahricandurucan@icloud.com>
2023-04-14 16:14:13 +0300

    recyclerview eklendi, listeleme yapıldı ve detay sayfasına geçiş ayarlandı

--- app/src/main/java/com/example/yemeksiparisuygulamasikotlin/ui/fragment/AnasayfaFragment.kt
+++ app/src/main/java/com/example/yemeksiparisuygulamasikotlin/ui/fragment/AnasayfaFragment.kt
@@ -7,8 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.yemeksiparisuygulamasikotlin.R
+import com.example.yemeksiparisuygulamasikotlin.data.entity.Yemekler
 import com.example.yemeksiparisuygulamasikotlin.databinding.FragmentAnasayfaBinding
+import com.example.yemeksiparisuygulamasikotlin.ui.adapter.YemeklerAdapter
 
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
@@ -17,9 +20,26 @@ class AnasayfaFragment : Fragment() {
 
         binding.toolbarAnasayfa.title = "Yemekler Listesi"
 
-        binding.button.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.yemekDetayGecis)
-        }
+        binding.rv.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+
+        val yemeklerListesi = ArrayList<Yemekler>()
+        val y1 = Yemekler(1,"Ayran","download",12)
+        val y2 = Yemekler(1,"Ayran","download",12)
+        val y3 = Yemekler(1,"Ayran","download",12)
+        val y4 = Yemekler(1,"Ayran","download",12)
+        val y5 = Yemekler(1,"Ayran","download",12)
+        val y6 = Yemekler(1,"Ayran","download",12)
+
+        yemeklerListesi.add(y1)
+        yemeklerListesi.add(y2)
+        yemeklerListesi.add(y3)
+        yemeklerListesi.add(y4)
+        yemeklerListesi.add(y5)
+        yemeklerListesi.add(y6)
+
+        val adapter = YemeklerAdapter(requireContext(),yemeklerListesi)
+        binding.rv.adapter = adapter
+
 
         return binding.root
     }
--- app/src/main/java/com/example/yemeksiparisuygulamasikotlin/ui/fragment/DetaySayfasiFragment.kt
+++ app/src/main/java/com/example/yemeksiparisuygulamasikotlin/ui/fragment/DetaySayfasiFragment.kt
@@ -7,19 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.example.yemeksiparisuygulamasikotlin.R
 import com.example.yemeksiparisuygulamasikotlin.databinding.FragmentDetaySayfasiBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DetaySayfasiFragment : Fragment() {
     private lateinit var binding: FragmentDetaySayfasiBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_detay_sayfasi, container, false)
 
-        binding.toolbarDetay.title = "Yemek Adı"
 
-        binding.button2.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.yemekSepetGecis)
+        val bundle:DetaySayfasiFragmentArgs by navArgs()
+        val gelenYemek = bundle.yemek
+
+        binding.toolbarDetay.title = "Yemek Adı"
+        binding.ivYemekResim.setImageResource(resources.getIdentifier(gelenYemek.yemek_resim_adi,"drawable",requireContext().packageName))
+        binding.tvYemekAdi.text = gelenYemek.yemek_adi
+        binding.tvYemekFiyat.text = gelenYemek.yemek_fiyat.toString()
+
+        binding.buttonSepetEkle.setOnClickListener {
+            Snackbar.make(it,"${gelenYemek.yemek_adi} sepete eklendi",Snackbar.LENGTH_LONG).show()
         }
+
 
         return binding.root
     }
